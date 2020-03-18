91aa6b5fbb878a4bf2769055f7254a562262da8b
Ryan Snodgrass <rsnodgrass@gmail.com>
2020-03-17 21:52:41 -0700

    updates

--- custom_components/lunos/__init__.py
+++ custom_components/lunos/__init__.py
@@ -3,8 +3,9 @@ LUNOS Ventilation Fan Control for Home Assistant
 https://github.com/rsnodgrass/hass-lunos
 """
 import logging
-import yaml
+
 import os
+import yaml
 
 from homeassistant.helpers import config_validation as cv, discovery
 from homeassistant.helpers.entity import Entity
@@ -22,7 +23,8 @@ LUNOS_DOMAIN = 'lunos'
 CFM_TO_CMHR = 1.69901 # 1 cubic feet/minute = 1.69901 cubic meters/hour
 
 # read the configuration that describes each of the LUNOS controller coding settings
-with open(r'custom_components/lunos/lunos-codings.yaml') as file:
+config_path = os.path.dirname(__file__)
+with open(rf"{config_path}/lunos-codings.yaml") as file:
     LUNOS_CODING_CONFIG = yaml.full_load(file)
  
 async def async_setup(hass, config):
