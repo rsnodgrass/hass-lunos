46f55bc5ad196ae45b91cce44eb24928034031c0
Ryan Snodgrass <rsnodgrass@gmail.com>
2020-03-17 21:54:53 -0700

    updated codings

--- custom_components/lunos/__init__.py
+++ custom_components/lunos/__init__.py
@@ -23,10 +23,15 @@ LUNOS_DOMAIN = 'lunos'
 CFM_TO_CMHR = 1.69901 # 1 cubic feet/minute = 1.69901 cubic meters/hour
 
 # read the configuration that describes each of the LUNOS controller coding settings
+LUNOS_CODING_CONFIG = {}
 config_path = os.path.dirname(__file__)
-with open(rf"{config_path}/lunos-codings.yaml") as file:
-    LUNOS_CODING_CONFIG = yaml.full_load(file)
- 
+config_file = rf"{config_path}/lunos-codings.yaml"
+try:
+    with open(config_file) as file:
+        LUNOS_CODING_CONFIG = yaml.full_load(file)
+except:
+    LOG.error(f"Failed to load LUNOS config {config_file}")
+
 async def async_setup(hass, config):
     LOG.info(f"LUNOS controller codings supported: {LUNOS_CODING_CONFIG.keys()}")
     return True
